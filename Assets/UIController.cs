0efca88093f391de8e60c5130b81e3703c176942
neko7890-THINK\neko7890 <nice.hermori@gmail.com>
2018-03-17 17:19:46 +0900

    家出ヤッタやつ

---
+++ Assets/UIController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    private GameObject gameOverText;
+
+    private Text runLengthText;
+
+    private float len = 0;
+
+    private float speed = 0.03f;
+
+    private bool isGameOver = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.gameOverText = GameObject.Find("GameOver");
+        this.runLengthText = GameObject.Find("RunLength").GetComponent<Text>();
+
+    }
+	// Update is called once per frame
+	void Update () {
+		if(this.isGameOver == false)
+        {
+            this.len += this.speed;
+            this.runLengthText.text = "Distance: "+ len.ToString("F2") + "m";
+        }
+        if (this.isGameOver)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+	}
+
+    public void GameOver()
+    {
+        this.gameOverText.GetComponent<Text>().text = "GameOver";
+        this.isGameOver = true;
+    }
+}
