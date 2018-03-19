1cd6f8d5e4c4c527173921c47bd8ce7d3d1667ac
takeru morimoto <nice.hermori@gmail.com>
2018-03-19 22:18:49 +0900

    teisyutuyou

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -8,11 +8,15 @@ public class CubeController : MonoBehaviour {
 
     private float deadLine = -10;
 
-	// Use this for initialization
-	void Start () {
-  
-	}
-	
+    AudioSource audioSource;
+
+    public AudioClip audioClip;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(this.speed, 0, 0);
@@ -22,4 +26,11 @@ public class CubeController : MonoBehaviour {
             Destroy(gameObject);
         }
 	}
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "cube" || other.gameObject.tag == "ground")
+        {
+            audioSource.PlayOneShot(audioClip , 1);
+        }
+    }
 }
